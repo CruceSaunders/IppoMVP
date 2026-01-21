7ccfbc517a01ce6bab5c94262b4ad8630f2b1084
Cruce <cruce.saunders@alpha.school>
2026-01-20 21:15:20 -0600

    Initial Commit

---
+++ IppoMVP/IppoMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  IppoMVPApp.swift
+//  IppoMVP
+//
+//  Created by Cruce Gauntlet on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct IppoMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
